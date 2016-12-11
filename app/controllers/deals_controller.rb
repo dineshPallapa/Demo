4b5063a55f2dbd2d2a5eb3a1e7e1e34235dfa168
dineshVirus <dinesh@enroco.com>
2016-12-11 16:25:39 +0530

    Added controllers for deals and surveys

---
+++ app/controllers/deals_controller.rb
@@ -0,0 +1,76 @@
+class DealsController < ApplicationController
+  before_action :find_deal,only: [:show,:edit,:update,:destroy]
+  before_action :authenticate_user!, only: [:new, :edit]
+
+  helper DealsHelper
+
+  def index
+    if params[:category].blank?
+    @deals = Deal.paginate(:page => params[:page],:per_page => 10).order("created_at desc")
+    else 
+      @category_id = Category.find_by(name: params[:category]).id
+      @deals = Deal.paginate(:page => params[:page],:per_page => 20).where(:category_id => @category_id)
+    end
+  end
+
+  def new 
+    @deal = current_user.deals.build
+    @categories = Category.all.map{ |c| [c.name, c.id] }
+  end
+
+  def create
+    @deal = current_user.deals.build(deal_params)
+    @deal.category_id = params[:category_id]
+    if @deal.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    if @deal.surveys.blank?
+      @average_surveys = 0
+    else
+      @average_surveys = @deal.surveys.average(:rating).round(2)
+    end
+  end 
+
+  def edit
+    @categories = Category.all.map{|c| [c.name,c.id]}
+  end
+
+  def update
+    if @deal.update(deal_params)
+      redirect_to @deal
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @deal.destroy
+      redirect_to root_path
+    end
+  end
+  
+  def searchable
+    if params[:search].blank?
+      redirect_to root_path
+    else
+      searchword = params[:search]
+      @data = Deal.where("title LIKE ? or description LIKE ?", "%#{searchword}%","%#{searchword}%")
+    end
+  end
+
+  private
+
+  def deal_params
+    params.require(:deal).permit(:title,:description,:link,:link1,:category_id,:image)
+  end
+
+  def find_deal
+    @deal = Deal.find(params[:id])
+  end
+
+end
---
+++ app/controllers/surveys_controller.rb
@@ -0,0 +1,52 @@
+class SurveysController < ApplicationController
+  before_action :find_deal
+  before_action :find_survey, only: [:show,:edit,:update,:destroy]
+  before_action :authenticate_user!, only: [:new, :edit]
+  def index
+
+  end
+  def new
+    @survey = Survey.new
+  end
+  def create
+    @survey = Survey.new(survey_params)
+    @survey.deal_id = @deal.id
+    @survey.user_id = current_user.id
+    if @survey.save
+      redirect_to deal_path(@deal)
+    else 
+      render :new
+    end
+  end
+  def show
+
+  end
+  def edit
+
+  end
+  def update
+    if @survey.update(survey_params)
+      redirect_to deal_path(@deal)
+    else 
+      render :edit
+    end
+
+  end
+  def destroy
+    @survey.destroy
+    redirect_to deal_path(@deal)
+  end
+
+  private
+    def survey_params
+      params.require(:survey).permit(:rating,:comment)
+    end
+
+    def find_deal
+      @deal = Deal.find(params[:deal_id])
+    end
+
+    def find_survey
+      @survey=Survey.find(params[:id])
+    end
+end
