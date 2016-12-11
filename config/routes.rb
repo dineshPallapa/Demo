bf805a77a18508ea8c433499ea60331e4e406671
dineshVirus <dinesh@enroco.com>
2016-12-11 15:06:22 +0530

    added restful routes for application

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :deals do 
+    resources :surveys
+  end
+  root to: 'deals#index'
+  get 'searchable' => 'deals#searchable'
 end
