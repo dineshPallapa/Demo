1a4c60843d29f624da21db1b2ff2d6e7ef1fcbb7
dineshPallapa <dinesh@enroco.com>
2017-01-27 05:59:53 +0530

    Added custom path for storing paperclip images

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -3,7 +3,10 @@ class Deal < ApplicationRecord
   belongs_to :category
   has_many :surveys
 
-  has_attached_file :image, styles: { large: "400x400>",  medium: "300x300>", thumb: "200x200>" }
+  has_attached_file :image, styles: { large: "400x400>",  medium: "300x300>", thumb: "200x200>"},
+                            :default_url => "/system/avatars/:style/missing.png",
+                            :url  => "/system/:attachment/:id/:style_:filename",
+                            :path => ":rails_root/public/system/:attachment/:id/:style_:filename"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
